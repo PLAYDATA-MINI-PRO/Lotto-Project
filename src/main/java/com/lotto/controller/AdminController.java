f749fa84d56af3bcbd7b69cf622a50b6448c5e06
icebear2n2 <icebear2n2@gmail.com>
2023-07-06 14:51:35 +0900

    feat: 회원가입 페이지 bootstrap 추가

--- src/main/java/com/lotto/controller/AdminController.java
+++ src/main/java/com/lotto/controller/AdminController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -85,4 +84,5 @@ public class AdminController {
 
         return "/admin/userMoneyList";
     }
+
 }
